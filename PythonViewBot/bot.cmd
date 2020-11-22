d44f10668f80b2b598f5f783a30cee48dd6630e9
LokeyaDisi <74868293+LokeyaDisi@users.noreply.github.com>
2020-11-22 23:34:35 +0800

    Create bot.cmd

---
+++ PythonViewBot/bot.cmd
@@ -0,0 +1,19 @@
+import time;
+from selenium import webdriver;
+
+#time to refresh page (seconds)
+Timer = 120
+
+#youtube link
+link = 'https://www.youtube.com/watch?v=zMCjylMndjo'
+
+#number of views
+views = 10
+
+driver = webdriver.Chrome()
+driver.get(link)
+
+for i in range(views):
+    time.sleep(Timer)
+    driver.refresh()
+    print(i)
